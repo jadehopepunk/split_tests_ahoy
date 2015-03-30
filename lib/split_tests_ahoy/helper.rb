b21d02eb685d082091bd98db968ad6df469985bf
Craig Ambrose <craig@craigambrose.com>
2015-03-30 13:47:53 +1300

    call track ahoy visit

--- lib/split_tests_ahoy/helper.rb
+++ lib/split_tests_ahoy/helper.rb
@@ -3,11 +3,7 @@ require 'split_tests_ahoy/visit'
 module SplitTestsAhoy
   module Helper
     def ahoy_split_test(experiment_name, *alternatives)
-
-    end
-
-    def ahoy_split_visit
-
+      track_ahoy_visit
     end
   end
 end
--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -1,15 +1,14 @@
 require 'spec_helper'
 require 'split_tests_ahoy/helper'
 
-describe SplitTestsAhoy::Helper do
+describe SplitTestsAhoy::Helper, :type => :controller  do
   include SplitTestsAhoy::Helper
 
   describe "split_test" do
     it "should ensure that a visitor has been created" do
-      ahoy_split_test('first_experiment', 'foo', 'bar')
+      expect(self).to receive(:track_ahoy_visit)
 
-      visit = ahoy_split_visit
-      expect(visit).to be_a(SplitTestsAhoy::Visit)
+      ahoy_split_test('first_experiment', 'foo', 'bar')
     end
   end
 end
