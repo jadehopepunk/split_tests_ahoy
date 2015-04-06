708d782621d0e3d0c201e372fb7244a967e67211
Craig Ambrose <craig@craigambrose.com>
2015-04-06 17:58:06 +1200

    fix migrations

--- db/migrate/20150330042231_create_participants.rb
+++ db/migrate/20150330042231_create_participants.rb
@@ -2,7 +2,7 @@ class CreateParticipants < ActiveRecord::Migration
   def change
     create_table :split_tests_ahoy_participants do |t|
       t.uuid :visit_id
-      t.id :experiment_id
+      t.integer :experiment_id
       t.string :alternative_name
       t.datetime :created_at
       t.datetime :ended_at
